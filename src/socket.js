7bd26befa98db564c75cb5de34dca010f80264d8
Mohd Hasan <Hasan@Mckinsol.local>
2026-01-14 20:47:32 +0530

    commit

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+// const socket = io("http://localhost:4000", {
+const socket = io("https://gigflow-backend-k53b.onrender.com", {
+
   withCredentials: true,
 });
 
